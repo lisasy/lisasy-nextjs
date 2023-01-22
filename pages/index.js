3ac1e0c9617a13d740627f1ae40b226b6dc333e6
Lisa Sy <lisasyis@gmail.com>
2023-01-21 21:43:38 -0800

    Add info to selected imgs

--- pages/index.js
+++ pages/index.js
@@ -48,12 +48,77 @@ export default function Home() {
           <h3 className={styles.sectionTitle}>
             Selected
           </h3>
-          <div className={styles.selectedList}>
-          <Image className={styles.selectedImage} src="/thumbnail-actus@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
-          <Image className={styles.selectedImage} src="/thumbnail-move@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
-          <Image className={styles.selectedImage} src="/thumbnail-ashapeoftheend@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
-          <Image className={styles.selectedImage} src="/thumbnail-fbreporting@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
-          <Image className={styles.selectedImage} src="/thumbnail-tallpoppy@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
+          <div >
+            <ul className={styles.selectedList}>
+              <li>
+                <article>
+                  <h4>Actus</h4>
+                  <p className={styles.itemDescription}>
+                    At Meta’s NPE, I led the design from 0-to-1 and brought to
+                    market a modern volunteering platform to bridge
+                    change-makers and community organizers.
+                  </p>
+                  <p className={styles.itemDate}>
+                    2022
+                  </p>
+                </article>
+                <Image className={styles.selectedImage} src="/thumbnail-actus@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
+              </li>
+              <li>
+                <article>
+                  <h4>Move</h4>
+                  <p className={styles.itemDescription}>
+                    At Meta's NPE, I brought to market Move, an social
+                    productivity app for empower community organizers and
+                    everyone alike to create tasks and stay organized.
+                  </p>
+                  <p className={styles.itemDate}>
+                    2021
+                  </p>
+                </article>
+                <Image className={styles.selectedImage} src="/thumbnail-move@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
+              </li>
+              <li>
+                <article>
+                  <h4>The Shape of the End</h4>
+                  <p className={styles.itemDescription}>
+                    During the COVID-19 pandemic lockdown, I created a comic to
+                  capture the pivotal moment and present a case for optimism
+                  looking ahead.
+                  </p>
+                  <p className={styles.itemDate}>
+                    2021
+                  </p>
+                </article>
+                <Image className={styles.selectedImage} src="/thumbnail-ashapeoftheend@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
+              </li>
+              <li>
+                <article>
+                  <h4>
+                    Facebook Live Reporting Experience
+                  </h4>
+                  <p className={styles.itemDescription}>
+                    I led design to release Live Video reporting from 0 to full release in 1 month. I coordinated with cross-functional partners in product, engineering, legal, and communications, across our Menlo Park and London timezones. Launched on Android, iOS, and web.
+                  </p>
+                  <p className={styles.itemDate}>
+                    2021
+                  </p>
+                </article>
+                <Image className={styles.selectedImage} src="/thumbnail-fbreporting@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
+              </li>
+              <li>
+                <article>
+                  <h4>Tall Poppy</h4>
+                  <p className={styles.itemDescription}>
+                    I supported Tall Poppy in revamping their product to help people proactively protect their online security and safety. I led research and design to improve new user onboarding and help people track the progress on actions they take to safeguard themselves.
+                  </p>
+                  <p className={styles.itemDate}>
+                    2019
+                  </p>
+                </article>
+                <Image className={styles.selectedImage} src="/thumbnail-tallpoppy@2x.png" alt="me" width={1920} height={1080} loading="lazy" />
+              </li>
+            </ul>
           </div>
         </section>
       </main>
@@ -72,6 +137,14 @@ export default function Home() {
           <li><a href="https://read.cv/lisasy" target="_blank" rel="noreferrer">Read.cv</a></li>
           <li><a href="https://twitter.com/lisasy" target="_blank" rel="noreferrer">Twitter</a></li>
         </ul>
+        <div className={styles.footerCredits}>
+          <p>
+            Made with <a href="https://nextjs.org/" target="_blank" rel="noreferrer">Next.js</a> and <a href="https://vercel.com/" target="_blank" rel="noreferrer">Vercel</a>. View my <a href="https://github.com/lisasy/lisasy-nextjs" target="_blank" rel="noreferrer">source code on Github</a>.
+          </p>
+          <p>
+            Typeface: Unbounded and DM Sans on Google Fonts.
+          </p>
+        </div>
       </footer>
     </div>
   )
